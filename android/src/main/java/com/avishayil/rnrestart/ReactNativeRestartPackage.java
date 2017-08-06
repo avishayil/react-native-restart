323232b79af36a0c678cf9721ab6b62d04ead860
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-08-06 21:11:45 +1200

    Revert "Remove overriden createJSModules"
    This reverts commit c7c412f472a55749ed9dc994c51323fc3202006b.

--- android/src/main/java/com/avishayil/rnrestart/ReactNativeRestartPackage.java
+++ android/src/main/java/com/avishayil/rnrestart/ReactNativeRestartPackage.java
@@ -24,6 +24,11 @@ public class ReactNativeRestartPackage implements ReactPackage {
     }
 
     @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return new ArrayList<>();
     }
