ff712fa3b3718af8d90e7e18f4d8eae7dc115545
Alberto Schiabel <jkomyno@users.noreply.github.com>
2017-09-15 15:40:56 +0200

    Deprecated createJSModules on Android only for RN >= 0.47

--- android/src/main/java/com/avishayil/rnrestart/ReactNativeRestartPackage.java
+++ android/src/main/java/com/avishayil/rnrestart/ReactNativeRestartPackage.java
@@ -22,7 +22,8 @@ public class ReactNativeRestartPackage implements ReactPackage {
         modules.add(new ReactNativeRestart(reactContext));
         return modules;
     }
-
+    
+    // Deprecated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
