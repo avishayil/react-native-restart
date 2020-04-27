b3e0141c8260798621717d54e5b635948c5d8bec
Avishay Bar <avishay.il@gmail.com>
2020-04-27 12:23:24 +0300

    feat: add unit test

---
+++ src/__mocks__/react-native-restart.ts
@@ -0,0 +1,3 @@
+export default {
+  Restart: jest.fn(),
+};
--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,1 +1,12 @@
-it.todo('write a test');
+/* eslint max-len: 0 */
+
+import 'react-native';
+import RNRestart from 'react-native-restart';
+
+jest.mock('react-native-restart');
+
+describe('test RNRestart API functions', () => {
+  it('calls the restart function', () => {
+    RNRestart.Restart();
+  });
+});
