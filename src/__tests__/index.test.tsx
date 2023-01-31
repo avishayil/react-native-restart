b162650ccfc064990ed30ce2585d29878d27cf49
Avishay Bar <avishay.il@gmail.com>
2023-01-31 09:26:20 +0200

    chore: updated dependencies, lint settings

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,10 +1,10 @@
 /* eslint max-len: 0 */
 
-import 'react-native';
-import RNRestart from 'react-native-restart';
+import "react-native";
+import RNRestart from "react-native-restart";
 
-describe('test RNRestart API functions', () => {
-  it('calls the restart function', () => {
-    RNRestart.Restart();
-  });
+describe("test RNRestart API functions", () => {
+    it("calls the restart function", () => {
+        RNRestart.Restart();
+    });
 });
