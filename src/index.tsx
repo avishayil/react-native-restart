1a7a0ad088e8f76ed6cd8f34c069673e416695c0
Amir Hossein Shekari <vanenshi@gmail.com>
2023-03-18 16:12:42 +0330

    docs: add typescript docs for deprecated function (#236)

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import { NativeModules } from "react-native";
 const { RNRestart } = NativeModules;
 
 type RestartType = {
+  /**
+   * @deprecated use `restart` instead
+   */
   Restart(): void;
   restart(): void;
 };
