73330c9b3f0ba27140b040105e8630a0ebeb61b1
Vlad Markuts <vlad@markuts.com>
2026-07-29 17:02:27 +0300

    fix(android): restore hard restart semantics

--- android/src/main/java/com/reactnativerestart/RestartModule.java
+++ android/src/main/java/com/reactnativerestart/RestartModule.java
@@ -1,123 +1,74 @@
 package com.reactnativerestart;
 
-import android.app.Activity;
-import android.os.Handler;
-import android.os.Looper;
-import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactInstanceManager;
-import com.facebook.react.bridge.LifecycleEventListener;
+import android.content.Context;
+import android.content.SharedPreferences;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.jakewharton.processphoenix.ProcessPhoenix;
-import com.facebook.react.bridge.Promise;
-
 
 public class RestartModule extends ReactContextBaseJavaModule {
 
-    private static final String REACT_APPLICATION_CLASS_NAME = "com.facebook.react.ReactApplication";
-    private static final String REACT_NATIVE_HOST_CLASS_NAME = "com.facebook.react.ReactNativeHost";
+    private static final String PREFERENCES_NAME = "react-native-restart";
+    private static final String RESTART_REASON_KEY = "restartReason";
     private static String restartReason = null;
-
-    private LifecycleEventListener mLifecycleEventListener = null;
+    private static boolean restartReasonInitialized = false;
 
     public RestartModule(ReactApplicationContext reactContext) {
         super(reactContext);
+        initializeRestartReason();
     }
 
-    private void loadBundleLegacy() {
-        final Activity currentActivity = getCurrentActivity();
-        if (currentActivity == null) {
+    private SharedPreferences getPreferences() {
+        return getReactApplicationContext().getSharedPreferences(
+            PREFERENCES_NAME,
+            Context.MODE_PRIVATE
+        );
+    }
+
+    private void initializeRestartReason() {
+        if (restartReasonInitialized) {
             return;
         }
 
-        currentActivity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                currentActivity.recreate();
-            }
-        });
+        SharedPreferences preferences = getPreferences();
+        if (preferences.contains(RESTART_REASON_KEY)) {
+            restartReason = preferences.getString(RESTART_REASON_KEY, null);
+            preferences.edit().remove(RESTART_REASON_KEY).commit();
+        }
+        restartReasonInitialized = true;
     }
 
-    private void loadBundle() {
-        clearLifecycleEventListener();
-        try {
-            final ReactInstanceManager instanceManager = resolveInstanceManager();
-            if (instanceManager == null) {
-                return;
-            }
+    private void restartApp(String reason) {
+        restartReason = reason;
+        SharedPreferences.Editor editor = getPreferences().edit();
 
-            new Handler(Looper.getMainLooper()).post(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        instanceManager.recreateReactContextInBackground();
-                    } catch (Throwable t) {
-                        loadBundleLegacy();
-                    }
-                }
-            });
-
-        } catch (Throwable t) {
-            loadBundleLegacy();
-        }
-    }
-
-    private static ReactInstanceHolder mReactInstanceHolder;
-
-    static ReactInstanceManager getReactInstanceManager() {
-        if (mReactInstanceHolder == null) {
-            return null;
-        }
-        return mReactInstanceHolder.getReactInstanceManager();
-    }
-
-    private ReactInstanceManager resolveInstanceManager() throws NoSuchFieldException, IllegalAccessException {
-        ReactInstanceManager instanceManager = getReactInstanceManager();
-        if (instanceManager != null) {
-            return instanceManager;
+        if (reason == null) {
+            editor.remove(RESTART_REASON_KEY);
+        } else {
+            editor.putString(RESTART_REASON_KEY, reason);
         }
 
-        final Activity currentActivity = getCurrentActivity();
-        if (currentActivity == null) {
-            return null;
-        }
-
-        ReactApplication reactApplication = (ReactApplication) currentActivity.getApplication();
-        instanceManager = reactApplication.getReactNativeHost().getReactInstanceManager();
-
-        return instanceManager;
-    }
-
-
-    private void clearLifecycleEventListener() {
-        if (mLifecycleEventListener != null) {
-            getReactApplicationContext().removeLifecycleEventListener(mLifecycleEventListener);
-            mLifecycleEventListener = null;
-        }
+        // ProcessPhoenix terminates the process immediately, so persist synchronously.
+        editor.commit();
+        ProcessPhoenix.triggerRebirth(getReactApplicationContext());
     }
 
     @ReactMethod
     public void Restart(String reason) {
-        restartReason = reason;
-        loadBundle();
+        restartApp(reason);
     }
 
     @ReactMethod
     public void restart(String reason) {
-        restartReason = reason;
-        loadBundle();
+        restartApp(reason);
     }
-    
+
     @ReactMethod
     public void getReason(Promise promise) {
-        try {
-            promise.resolve(restartReason);
-        } catch(Exception e) {
-            promise.reject("Create Event Error", e);
-        }
+        promise.resolve(restartReason);
     }
-    
 
     @Override
     public String getName() {
